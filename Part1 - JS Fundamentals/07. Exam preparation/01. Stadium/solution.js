1cd0c53cec33b68822424400611b3a32846a1fc1
AChristoff <aleksov101@gmail.com>
2019-02-09 00:12:13 +0200

    JS-DOM-Fundamentals

--- Part1 - JS Fundamentals/07. Exam preparation/01. Stadium/solution.js
+++ Part1 - JS Fundamentals/07. Exam preparation/01. Stadium/solution.js
@@ -1,61 +1,32 @@
 function solve() {
 
-    let allSeats = Array.from(document.querySelectorAll('tbody button'));
+    let seatButtons = Array.from(document.querySelectorAll('.seat')).forEach((btn) => btn.addEventListener('click', selectSeat));
     let output = document.querySelector('#output');
 
-    let fansCounter = 0;
-    let profit = 0;
+    let seatsInfo = {
+        'Levski': {'A': 10, 'B': 7, 'C': 5},
+        'Litex': {'A': 10, 'B': 7, 'C': 5},
+        'VIP': {'A': 25, 'B': 15, 'C': 10},
+        'results': {'profit': 0, 'soldTickets': 0}
+    };
 
-    let secotrsA = seatsBySector(allSeats, 0);
-    let secotrsB = seatsBySector(allSeats, 1);
-    let secotrsC = seatsBySector(allSeats, 2);
+    function selectSeat(e) {
+        let seat = e.target;
+        let zone = seat.parentElement.parentElement.parentElement.parentElement.parentElement.className;
+        let sector = String.fromCharCode(65 + seat.parentElement.cellIndex);
 
-    reserveAndCollect(secotrsA, 'A');
-    reserveAndCollect(secotrsB, 'B');
-    reserveAndCollect(secotrsC, 'C');
-
-    function seatsBySector(seats, indexManipulator) {
-        let sector = seats.reduce((acc, seat, idx) => {
-            if ((idx - indexManipulator) % 3 === 0) {
-                acc.push(seat);
-            }
-            return acc;
-        }, []);
-
-        let sectors = [];
-        spliceAndPush('zoneLevski');
-        spliceAndPush('zoneLitex');
-        spliceAndPush('zoneVIP');
-
-        function spliceAndPush(zone) {
-            zone = sector.splice(0, 5);
-            sectors.push(zone);
-        }
-        return sectors
-    }
-    function reserveAndCollect(seatsBySector, sector) {
-
-        let zones = ['Levski', 'Litex', 'VIP'];
-        let prices = {'A': [10, 10, 25], 'B': [7, 7, 15], 'C': [5, 5, 10]};
-
-        for (let i = 0; i < 3; i++) {
-
-            seatsBySector[i].forEach((x) => x.addEventListener('click', () => {
-
-                if (!x.style.backgroundColor) {
-                    x.style.backgroundColor = 'rgb(255,0,0)';
-                    profit += prices[`${sector}`][i];
-                    fansCounter++;
-                    output.value += ` Seat ${x.textContent} in zone ${zones[i]} sector ${sector} was taken.\n`
-                } else {
-                    output.value += ` Seat ${x.textContent} in zone ${zones[i]} sector ${sector} is unavailable.\n`
-                }
-            }));
+        if (!seat.style.backgroundColor) {
+            seat.style.backgroundColor = 'rgb(255,0,0)';
+            seatsInfo.results.profit += seatsInfo[zone][sector];
+            seatsInfo.results.soldTickets++;
+            output.value += ` Seat ${seat.textContent} in zone ${zone} sector ${sector} was taken.\n`
+        } else {
+            output.value += ` Seat ${seat.textContent} in zone ${zone} sector ${sector} is unavailable.\n`
         }
     }
 
     let summaryBtn = document.querySelector('#summary button').addEventListener('click', showResult => {
         let spanElement = document.querySelector('#summary span');
-        spanElement.textContent = `${profit} leva, ${fansCounter} fans.`;
+        spanElement.textContent = `${seatsInfo.results.profit} leva, ${seatsInfo.results.soldTickets} fans.`;
     });
 }
