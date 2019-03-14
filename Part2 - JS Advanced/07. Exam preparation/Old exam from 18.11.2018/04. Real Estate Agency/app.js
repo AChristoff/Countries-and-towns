dae3d6616acb4769bc4f0e24b77408434d5373a6
AChristoff <aleksov101@gmail.com>
2019-03-14 11:05:33 +0200

    Exam prep

--- Part2 - JS Advanced/07. Exam preparation/Old exam from 18.11.2018/04. Real Estate Agency/app.js
+++ Part2 - JS Advanced/07. Exam preparation/Old exam from 18.11.2018/04. Real Estate Agency/app.js
@@ -6,20 +6,18 @@ function realEstateAgency() {
 
     regBtn.on('click', () => {
 
-        let price = $('[name="apartmentRent"]').val();
-        let type = $('[name="apartmentType"]').val();
-        let commission = $('[name="agencyCommission"]').val();
+        let offerInfo = $('#regOffer input').toArray().map((x) => $(x));
+        let [price, type, commission] = offerInfo;
         let offers = $('#building');
 
-        if (isNaN(type) && +price > 0 && +commission >= 0 && +commission <= 100) {
+        if (isNaN(type.val()) && +price.val() > 0 && +commission.val() >= 0 && +commission.val() <= 100) {
 
             let offerTags = ['Rent', 'Type', 'Commission'];
-            let offerInfo = $('#regOffer input').toArray().map((x) => $(x).val());
             let div = $('<div class="apartment"></div>');
 
             for (let i = 0; i < offerInfo.length; i++) {
                 let p = $('<p>');
-                p.text(`${offerTags[i]}: ${offerInfo[i]}`);
+                p.text(`${offerTags[i]}: ${offerInfo[i].val()}`);
                 p.appendTo(div);
             }
 
@@ -30,21 +28,19 @@ function realEstateAgency() {
             notification.text('Your offer registration went wrong, try again.');
         }
 
-        $('[name="apartmentRent"]').val('');
-        $('[name="apartmentType"]').val('');
-        $('[name="agencyCommission"]').val('');
+        price.val('');
+        type.val('');
+        commission.val('')
     });
 
     findBtn.on('click', () => {
 
-        let budget = $('[name="familyBudget"]').val();
-        let searchedType = $('[name="familyApartmentType"]').val();
-        let familyName = $('[name="familyName"]').val();
+        let [budget, searchedType, familyName] = $('#findOffer input').toArray().map((x) => $(x).val());
         let agencyProfitReport = $('#roof h1');
         let agencyProfit = +$('#roof h1').text().split(' ')[2];
+        let offers = $('.apartment').toArray();
         let prices = $('.apartment p:nth-child(1)').toArray().map((x) => $(x).text().split(' ')[1]);
         let types = $('.apartment p:nth-child(2)').toArray().map((x) => $(x).text().split(' ')[1]);
-        let offers = $('.apartment').toArray();
         let commissionPercent = $('.apartment p:nth-child(3)').toArray().map((x) => $(x).text().split(' ')[1]);
 
         if (+budget > 0 && searchedType && familyName) {
@@ -52,8 +48,10 @@ function realEstateAgency() {
             for (let i = 0; i < offers.length; i++) {
 
                 function isTaken(offerIndex) {
-                    if (isNaN(+prices[offerIndex]) && offerIndex < offers.length) {
+                    if (isNaN(+prices[offerIndex])) {
+                        if (offerIndex < offers.length) {
                             return isTaken(offerIndex+1)
+                        }
                     }
                     return offerIndex;
                 }
@@ -64,18 +62,18 @@ function realEstateAgency() {
 
                 if (totalPrice <= budget && searchedType === types[isTaken(i)]) {
 
-                    let apartment = $(offerRows[0]).parent();
+                    let offer = $(offerRows[0]).parent();
                     let moveOutBtn = $('<button>MoveOut</button>');
                     moveOutBtn.on('click', () => {
-                        apartment.remove();
+                        offer.remove();
                         notification.text(`They had found cockroaches in ${familyName}'s apartment`);
                     });
 
-                    apartment.attr('style', 'border: 2px solid red');
+                    offer.attr('style', 'border: 2px solid red');
                     $(offerRows[0]).text(familyName);
                     $(offerRows[1]).text('live here now');
                     $(offerRows[2]).remove();
-                    moveOutBtn.appendTo(apartment);
+                    moveOutBtn.appendTo(offer);
 
                     agencyProfitReport.text(`Agency profit: ${agencyProfit + currentCommission * 2} lv.`);
                     notification.text('Enjoy your new home! :))');
